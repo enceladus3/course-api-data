347deb180d162b4a06d74b27063d9f0d29a9806b
oct-mbp <oct-mbp@192-168-0-129.rdsnet.ro>
2020-03-26 22:19:30 +0200

    Initial commit

---
+++ src/main/java/com/example/courseapidata/topic/TopicService.java
@@ -0,0 +1,56 @@
+package com.example.courseapidata.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	@Autowired
+	private TopicRepository topicRepository;
+
+//	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+// 			new Topic("spring", "Spring Framework", "Spring Framework description"),
+//			new Topic("java", "Java Code", "Java Core description"),
+//			new Topic("javascript", "JavaScript", "JavaScript description")
+//			));
+//
+	public List<Topic> getAllTopics() {
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+//		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id).orElse(null);
+	}
+
+	public void addTopic(Topic topic) {
+		topicRepository.save(topic);
+
+	}
+
+	public void updateTopic(String id, Topic topic) {
+//		for (int i = 0; i < topics.size(); i++) {
+////			Topic t = topics.get(i);
+//			if (topics.get(i).getId().equals(id)) {
+//				topics.set(i, topic);
+//				return;
+//			}
+//		}
+		topicRepository.save(topic);
+
+	}
+
+	public void deleteTopic(String id) {
+//		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
+	}
+
+}
